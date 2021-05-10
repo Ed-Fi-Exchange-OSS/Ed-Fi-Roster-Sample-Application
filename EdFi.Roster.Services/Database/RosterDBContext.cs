d5c2efe90b40d61895d86fc3b0eb2073bf3b3949
Suganya <suganya.rajendran@instructure.com>
2021-05-10 16:44:18 -0500

    Define database context  and entity classes for SQLite

---
+++ EdFi.Roster.Services/Database/RosterDBContext.cs
@@ -0,0 +1,20 @@
+﻿using EdFi.Roster.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EdFi.Roster.Services.Database
+{
+    public class RosterDbContext : DbContext
+    {
+        public RosterDbContext(DbContextOptions<RosterDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<RosterLocalEducationAgency> LocalEducationAgencies { get; set; }
+        public DbSet<RosterSchool> Schools { get; set; }
+        public DbSet<RosterSection> Sections { get; set; }
+        public DbSet<RosterStaff> Staff { get; set; }
+        public DbSet<RosterStudent> Students { get; set; }
+    }
+}
