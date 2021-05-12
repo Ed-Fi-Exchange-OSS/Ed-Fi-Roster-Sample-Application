5eeb8c588c72e9e286ce285e1444577aedb78ae3
Saagar Minocha <saagar.minocha93@gmail.com>
2021-05-12 14:32:43 -0500

    Moved ApiClient.InterceptRequest.cs and ApiClient.InterceptResponse.cs to a folder named Customizations in the same project to avoid mistaking them to be a part of the code generation process.

--- EdFi.Roster.Sdk/Customizations/ApiClient.InterceptResponse.cs
+++ EdFi.Roster.Sdk/Customizations/ApiClient.InterceptResponse.cs
@@ -1,12 +1,5 @@
 ﻿using EdFi.Roster.Sdk.Services;
 using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EdFi.Roster.Sdk.Client
 {
